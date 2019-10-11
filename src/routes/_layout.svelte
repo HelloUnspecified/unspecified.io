1e32fae758ac489f126a01a7d84089ed9499ecca
Clark Sell <clark.sell@csell.net>
2019-10-11 16:49:59 -0500

    inital commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,29 @@
+<script>
+  import ApolloClient from "apollo-boost";
+  import { setClient } from "svelte-apollo";
+
+  import Nav from "../components/Nav.svelte";
+  export let segment;
+
+  const client = new ApolloClient({
+    uri: "https://sandbarandislandgrill.components/api"
+  });
+  setClient(client);
+</script>
+
+<style>
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+</style>
+
+<Nav {segment} />
+
+<main>
+  <slot />
+</main>
