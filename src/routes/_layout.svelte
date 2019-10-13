2997003bd000fc70c6dbe8f6328e6227da8d5adf
Sara Gibbons <saragibby@gmail.com>
2019-10-13 15:10:17 -0400

    adds ability to set screen size as name on class

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,17 +2,39 @@
   import ApolloClient from "apollo-boost";
   import { onMount } from 'svelte';
   import Media from "../utils/Media.svelte";
+  import { screenSizes } from "../utils/mediaQuery";
   import { setClient } from "svelte-apollo";
 
   import Footer from "../components/Footer/Footer.svelte";
   import MobileFooter from "../components/Footer/MobileFooter.svelte";
   import Nav from "../components/Nav.svelte";
+
   export let segment;
 
   const client = new ApolloClient({
     uri: "https://sandbarandislandgrill.components/api"
   });
+  
+
+  let screenSize = "";
   setClient(client);
+
+  onMount(async () => {
+    const resized = () => {
+      let width = window.innerWidth;
+
+      Object.keys(screenSizes.screens).find((screen) => {
+        if(width >= screenSizes.screens[screen].min && width <= screenSizes.screens[screen].max) { 
+          screenSize = screen;
+        };
+      });
+      ;
+    }
+
+    resized();
+    window.addEventListener('resize', resized);
+  })
+  
 </script>
 
 <style>
@@ -24,11 +46,15 @@
     margin: 0 auto;
     box-sizing: border-box;
   }
+
+  main.xsmall {
+    padding-bottom: 8rem;
+  }
 </style>
 
 <Nav {segment} />
 
-<main>
+<main class={screenSize}>
   <slot />
 </main>
 
