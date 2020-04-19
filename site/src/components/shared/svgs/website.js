005b87833c10077e7cc553f02d88729d278657cf
Sara Gibbons <saragibby@gmail.com>
2020-04-19 13:47:07 -0400

    add technologies platforms block
    includes schema for testimonials

---
+++ site/src/components/shared/svgs/website.js
@@ -0,0 +1,51 @@
+import React from "react"
+
+const Website = ({ fill, height, width }) => {
+  return (
+    <svg
+      height={height}
+      width={width}
+      fill={fill}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      x="0px"
+      y="0px"
+      viewBox="0 0 64 64"
+      // style="enable-background:new 0 0 64 64;"
+      xmlSpace="preserve"
+    >
+      <g>
+        <path d="M45,55h16v3c0,1.654-1.346,3-3,3H6c-1.654,0-3-1.346-3-3v-3h16v-2H1v5c0,2.757,2.243,5,5,5h52c2.757,0,5-2.243,5-5v-5H45   V55z"></path>
+        <path d="M24,59h16c1.654,0,3-1.346,3-3v-3H21v3C21,57.654,22.346,59,24,59z M23,55h18v1c0,0.552-0.449,1-1,1H24   c-0.551,0-1-0.448-1-1V55z"></path>
+        <rect x="7" y="57" width="2" height="2"></rect>
+        <rect x="11" y="57" width="2" height="2"></rect>
+        <rect x="15" y="57" width="2" height="2"></rect>
+        <path d="M58,11h-3v2h3c1.654,0,3,1.346,3,3v35h2V16C63,13.243,60.757,11,58,11z"></path>
+        <path d="M3,16c0-1.654,1.346-3,3-3h3v-2H6c-2.757,0-5,2.243-5,5v35h2V16z"></path>
+        <path d="M57,51h2V17c0-1.103-0.897-2-2-2h-2v2h2V51z"></path>
+        <path d="M5,17v34h2V17h2v-2H7C5.897,15,5,15.897,5,17z"></path>
+        <path d="M53,1H11v10h42V1z M51,9H13V3h38V9z"></path>
+        <polygon points="53,13 51,13 51,47 13,47 13,13 11,13 11,49 53,49  "></polygon>
+        <rect x="15" y="5" width="2" height="2"></rect>
+        <rect x="19" y="5" width="2" height="2"></rect>
+        <rect x="23" y="5" width="2" height="2"></rect>
+        <path d="M28.05,18.536L28,18.586l-0.05-0.05C26.959,17.545,25.643,17,24.243,17C21.352,17,19,19.352,19,22.243   c0,1.4,0.545,2.717,1.536,3.707L28,33.414l7.464-7.464c0.99-0.99,1.536-2.307,1.536-3.707C37,19.352,34.648,17,31.757,17   C30.357,17,29.041,17.545,28.05,18.536z M35,22.243c0,0.866-0.337,1.68-0.95,2.293L28,30.586l-6.05-6.05   C21.337,23.923,21,23.109,21,22.243C21,20.455,22.455,19,24.243,19c0.866,0,1.68,0.337,2.293,0.95L28,21.414l1.464-1.464   c0.613-0.612,1.427-0.95,2.293-0.95C33.545,19,35,20.455,35,22.243z"></path>
+        <polygon points="41,13 15,13 15,33 17,33 17,15 41,15  "></polygon>
+        <polygon points="15,37 41,37 41,17 39,17 39,35 15,35  "></polygon>
+        <rect x="43" y="13" width="6" height="2"></rect>
+        <rect x="43" y="17" width="6" height="2"></rect>
+        <rect x="43" y="21" width="6" height="2"></rect>
+        <rect x="43" y="25" width="6" height="2"></rect>
+        <rect x="43" y="29" width="6" height="2"></rect>
+        <rect x="43" y="33" width="6" height="2"></rect>
+        <rect x="15" y="39" width="2" height="2"></rect>
+        <rect x="19" y="39" width="30" height="2"></rect>
+        <rect x="47" y="43" width="2" height="2"></rect>
+        <rect x="15" y="43" width="30" height="2"></rect>
+      </g>
+    </svg>
+  )
+}
+
+export default Website
---
+++ site/src/components/technologies.js
@@ -0,0 +1,105 @@
+import React from "react"
+import styled from "styled-components"
+import Cloud from "./shared/svgs/cloud"
+import Device from "./shared/svgs/device"
+import Mobile from "./shared/svgs/mobile"
+import Website from "./shared/svgs/website"
+import { colors } from "../utilities"
+
+const PLATFORM_ICON_SIZE = "8rem"
+
+const Platform = styled.div`
+  width: 15rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    font-size: 2.2rem;
+  }
+`
+
+const TechnologyBlock = styled.div`
+  border-top: 0.1rem solid ${colors.gold};
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 2rem;
+  position: relative;
+  padding-top: 3rem;
+`
+
+const TechnologyLogo = styled.img`
+  height: 6rem;
+  width: auto;
+  margin: 1rem 2rem;
+`
+
+const Technologies = ({ technologies }) => {
+  return (
+    <div>
+      <p style={{ lineHeight: "1.9" }}>
+        We don't just build products, we build solutions to solve problems. To
+        make experiences better. To bring you and your business more closely
+        connected to your customer across all the platforms you operate on.
+      </p>
+      <div
+        style={{
+          display: "flex",
+          marginTop: "3rem",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Platform key="website">
+          <Website
+            height={PLATFORM_ICON_SIZE}
+            width={PLATFORM_ICON_SIZE}
+            fill={colors.navy}
+          />
+          <p>Website</p>
+        </Platform>
+        <Platform key="device">
+          <Device
+            height={PLATFORM_ICON_SIZE}
+            width={PLATFORM_ICON_SIZE}
+            fill={colors.navy}
+          />
+          <p>Device</p>
+        </Platform>
+        <Platform key="mobile">
+          <Mobile
+            height={PLATFORM_ICON_SIZE}
+            width={PLATFORM_ICON_SIZE}
+            fill={colors.navy}
+          />
+          <p>Mobile</p>
+        </Platform>
+
+        <Platform key="cloud">
+          <Cloud
+            height={PLATFORM_ICON_SIZE}
+            width={PLATFORM_ICON_SIZE}
+            fill={colors.navy}
+          />
+          <p>Cloud</p>
+        </Platform>
+      </div>
+      <TechnologyBlock>
+        <p style={{ lineHeight: "1.9" }}>
+          Always reaching for the right tool for job we are constantly pushing
+          the envelope of new technologies and stacks. Our experience runs deep
+          through decades of engineering experience but here is a glimpse at
+          what we are currently passionate about...
+        </p>
+        {technologies.map(technology => (
+          <TechnologyLogo
+            src={technology.node.logo.asset.url}
+            key={technology.node.name}
+          />
+        ))}
+      </TechnologyBlock>
+    </div>
+  )
+}
+
+export default Technologies
